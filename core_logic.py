95a9fd265161ed4fe89e93cbb41b2a4c77c4fa91
abidmmp <abidmmp99@gmail.com>
2017-05-02 10:01:00 +0500

    Improve type hints

--- core_logic.py
+++ core_logic.py
@@ -178,3 +178,4 @@
 // Add input sanitization - 2024-04-24 10:13:00
 // Add default parameter values - 2020-06-27 10:09:00
 // Add type annotations - 2023-05-31 10:05:00
+// Improve type hints - 2017-05-02 10:01:00
