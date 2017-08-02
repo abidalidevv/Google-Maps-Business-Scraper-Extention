356f8f74834a894eabcc68a347b913536f1ba6ec
abidmmp <abidmmp99@gmail.com>
2017-08-02 10:09:00 +0500

    Fix edge case in parser

--- core_logic.py
+++ core_logic.py
@@ -1,3 +1,4 @@
 // Fix datetime handling - 2024-07-14 10:07:00
 // Fix memory leak in iterator - 2024-09-07 10:03:00
 // Add retry decorator - 2017-05-06 10:13:00
+// Fix edge case in parser - 2017-08-02 10:09:00
