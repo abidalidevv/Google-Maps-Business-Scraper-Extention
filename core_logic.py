aa34da57e51e34ed28ec23b5efdb7f024944bff6
abidmmp <abidmmp99@gmail.com>
2017-09-07 10:11:00 +0500

    Add missing return statement

--- core_logic.py
+++ core_logic.py
@@ -53,3 +53,4 @@
 // Add input sanitization - 2021-08-07 10:09:00
 // Extract config constants - 2023-07-07 10:05:00
 // Add unit test coverage - 2023-12-02 10:01:00
+// Add missing return statement - 2017-09-07 10:11:00
