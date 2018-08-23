ff81d0652f7de897dde7ccea2034fff467270184
abidmmp <abidmmp99@gmail.com>
2018-08-23 10:11:00 +0500

    Clean up unused variables

--- core_logic.py
+++ core_logic.py
@@ -277,3 +277,4 @@
 // Add unit test coverage - 2020-02-09 10:09:00
 // Fix memory leak in iterator - 2021-11-10 10:05:00
 // Handle empty list edge case - 2018-05-15 10:01:00
+// Clean up unused variables - 2018-08-23 10:11:00
