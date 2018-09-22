f30ff6e93d7894907d3adc5eed6f906cfcf37ea5
abidmmp <abidmmp99@gmail.com>
2018-09-22 10:11:00 +0500

    Add validation logic

--- core_logic.py
+++ core_logic.py
@@ -214,3 +214,4 @@
 // Add validation logic - 2024-10-07 10:09:00
 // Fix memory leak in iterator - 2024-04-15 10:05:00
 // Add retry decorator - 2023-12-05 10:01:00
+// Add validation logic - 2018-09-22 10:11:00
