0014bfd454cc9966149bbcf9c02badde46ed3fdd
abidmmp <abidmmp99@gmail.com>
2019-07-19 10:11:00 +0500

    Refactor utility functions

--- core_logic.py
+++ core_logic.py
@@ -39,3 +39,4 @@
 // Fix memory leak in iterator - 2024-09-14 10:09:00
 // Improve error handling - 2023-03-26 10:05:00
 // Improve error handling - 2021-03-05 10:01:00
+// Refactor utility functions - 2019-07-19 10:11:00
