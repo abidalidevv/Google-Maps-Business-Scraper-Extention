3d9dde3a490fee98e733464021ab183fb9c8962c
abidmmp <abidmmp99@gmail.com>
2019-07-30 10:05:00 +0500

    Simplify conditional logic

--- core_logic.py
+++ core_logic.py
@@ -2,3 +2,4 @@
 // Fix memory leak in iterator - 2024-09-07 10:03:00
 // Add retry decorator - 2017-05-06 10:13:00
 // Fix edge case in parser - 2017-08-02 10:09:00
+// Simplify conditional logic - 2019-07-30 10:05:00
