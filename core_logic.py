6b8ec9cf6c7b98f16e268eccae00c01131e50376
abidmmp <abidmmp99@gmail.com>
2020-03-25 10:03:00 +0500

    Remove dead code

--- core_logic.py
+++ core_logic.py
@@ -230,3 +230,4 @@
 // Fix circular import - 2017-08-03 10:01:00
 // Add type annotations - 2021-09-25 10:11:00
 // Add retry decorator - 2021-08-08 10:07:00
+// Remove dead code - 2020-03-25 10:03:00
