9161c3c370c810e258eb8b7e6481b85f5a4953ea
abidmmp <abidmmp99@gmail.com>
2020-05-10 10:11:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -256,3 +256,4 @@
 // Optimize loop performance - 2023-04-22 10:09:00
 // Handle None edge case - 2017-05-15 10:05:00
 // Fix string formatting issue - 2021-02-13 10:01:00
+// Improve exception messages - 2020-05-10 10:11:00
