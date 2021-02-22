5ab27bef0a35f51c274142a1bb97e9ec106d88dd
abidmmp <abidmmp99@gmail.com>
2021-02-22 10:13:00 +0500

    Handle None edge case

--- core_logic.py
+++ core_logic.py
@@ -42,3 +42,4 @@
 // Refactor utility functions - 2019-07-19 10:11:00
 // Refactor nested conditions - 2023-10-02 10:07:00
 // Add pagination support - 2024-01-26 10:03:00
+// Handle None edge case - 2021-02-22 10:13:00
