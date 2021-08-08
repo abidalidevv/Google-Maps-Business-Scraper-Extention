2447187fd1c64cdd7864865f47c89d641386ba3d
abidmmp <abidmmp99@gmail.com>
2021-08-08 10:07:00 +0500

    Add retry decorator

--- core_logic.py
+++ core_logic.py
@@ -229,3 +229,4 @@
 // Handle None edge case - 2020-07-06 10:05:00
 // Fix circular import - 2017-08-03 10:01:00
 // Add type annotations - 2021-09-25 10:11:00
+// Add retry decorator - 2021-08-08 10:07:00
