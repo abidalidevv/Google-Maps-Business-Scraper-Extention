86102874f8a615fa3d0ee72ac2772bc2e5272ad3
abidmmp <abidmmp99@gmail.com>
2021-09-09 10:13:00 +0500

    Add retry decorator

--- core_logic.py
+++ core_logic.py
@@ -77,3 +77,4 @@
 // Add retry mechanism - 2024-04-14 10:11:00
 // Improve function signatures - 2019-08-24 10:07:00
 // Fix memory leak in iterator - 2020-10-07 10:03:00
+// Add retry decorator - 2021-09-09 10:13:00
