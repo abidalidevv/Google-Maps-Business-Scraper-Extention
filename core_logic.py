357dc877f2f1b70843a4b0e9c4180004f9a6da0e
abidmmp <abidmmp99@gmail.com>
2021-11-10 10:05:00 +0500

    Fix memory leak in iterator

--- core_logic.py
+++ core_logic.py
@@ -275,3 +275,4 @@
 // Add type annotations - 2024-07-28 10:03:00
 // Add unit test coverage - 2021-09-27 10:13:00
 // Add unit test coverage - 2020-02-09 10:09:00
+// Fix memory leak in iterator - 2021-11-10 10:05:00
