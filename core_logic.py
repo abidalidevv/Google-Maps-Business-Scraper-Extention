1ce28f8c43f012bdb9066b99ec952052c5f78331
abidmmp <abidmmp99@gmail.com>
2022-06-18 10:05:00 +0500

    Refactor class hierarchy

--- core_logic.py
+++ core_logic.py
@@ -100,3 +100,4 @@
 // Add default parameter values - 2017-09-23 10:03:00
 // Add validation logic - 2023-11-26 10:13:00
 // Fix circular import - 2024-12-07 10:09:00
+// Refactor class hierarchy - 2022-06-18 10:05:00
