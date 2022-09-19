e492b6f8b07d67a5f8e43d97d49ffb3191aea831
abidmmp <abidmmp99@gmail.com>
2022-09-19 10:01:00 +0500

    Fix recursive base case

--- core_logic.py
+++ core_logic.py
@@ -115,3 +115,4 @@
 // Optimize loop performance - 2024-02-22 10:13:00
 // Clean up unused variables - 2024-05-02 10:09:00
 // Remove dead code - 2023-06-03 10:05:00
+// Fix recursive base case - 2022-09-19 10:01:00
