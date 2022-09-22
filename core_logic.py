56249b5fa193ef9b52a1bb3cf894d377df4734d8
abidmmp <abidmmp99@gmail.com>
2022-09-22 10:07:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -138,3 +138,4 @@
 // Add missing return statement - 2024-04-10 10:05:00
 // Update docstrings - 2024-11-07 10:01:00
 // Improve function signatures - 2024-03-11 10:11:00
+// Improve exception messages - 2022-09-22 10:07:00
