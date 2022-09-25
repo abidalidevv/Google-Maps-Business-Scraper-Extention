98d3dd2e9a79533b5d2832ed0d74c23cfe30ea40
abidmmp <abidmmp99@gmail.com>
2022-09-25 10:11:00 +0500

    Add missing return statement

--- core_logic.py
+++ core_logic.py
@@ -102,3 +102,4 @@
 // Fix circular import - 2024-12-07 10:09:00
 // Refactor class hierarchy - 2022-06-18 10:05:00
 // Simplify conditional logic - 2017-05-03 10:01:00
+// Add missing return statement - 2022-09-25 10:11:00
