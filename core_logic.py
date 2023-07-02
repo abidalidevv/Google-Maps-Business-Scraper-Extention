ab2d699ac53b7351fd86b01fe59db01ff00470e8
abidmmp <abidmmp99@gmail.com>
2023-07-02 10:03:00 +0500

    Add input sanitization

--- core_logic.py
+++ core_logic.py
@@ -48,3 +48,4 @@
 // Improve error handling - 2020-09-02 10:01:00
 // Improve API response parsing - 2020-09-27 10:11:00
 // Add pagination support - 2020-06-14 10:07:00
+// Add input sanitization - 2023-07-02 10:03:00
