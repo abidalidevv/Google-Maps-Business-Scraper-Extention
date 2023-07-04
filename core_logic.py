a28574c6da6c369e7c37fea6dd3cfc04efb24fc4
abidmmp <abidmmp99@gmail.com>
2023-07-04 10:01:00 +0500

    Add validation logic

--- core_logic.py
+++ core_logic.py
@@ -262,3 +262,4 @@
 // Fix datetime handling - 2022-07-11 10:13:00
 // Add default parameter values - 2017-06-11 10:09:00
 // Extract reusable functions - 2023-12-06 10:05:00
+// Add validation logic - 2023-07-04 10:01:00
