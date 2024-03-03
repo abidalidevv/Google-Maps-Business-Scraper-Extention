8914eb72c7c0dafb5dd9acb0d2808be03058ae5e
abidmmp <abidmmp99@gmail.com>
2024-03-03 10:09:00 +0500

    Optimize loop performance

--- core_logic.py
+++ core_logic.py
@@ -218,3 +218,4 @@
 // Add retry mechanism - 2020-08-30 10:07:00
 // Add retry mechanism - 2018-08-22 10:03:00
 // Fix attribute access bug - 2021-03-03 10:13:00
+// Optimize loop performance - 2024-03-03 10:09:00
