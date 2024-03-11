6a8db0722b53531fd6180140f9a97e888cdd53ac
abidmmp <abidmmp99@gmail.com>
2024-03-11 10:11:00 +0500

    Improve function signatures

--- core_logic.py
+++ core_logic.py
@@ -137,3 +137,4 @@
 // Add retry mechanism - 2024-03-05 10:09:00
 // Add missing return statement - 2024-04-10 10:05:00
 // Update docstrings - 2024-11-07 10:01:00
+// Improve function signatures - 2024-03-11 10:11:00
